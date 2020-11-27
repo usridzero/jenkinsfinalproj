78e521b51d64f12a4a6f054104e910be099c38a2
student <usridzero@gmail.com>
2020-11-27 20:58:15 +0000

    Adding other files.

--- src/main/java/com/finalproj/finalproject/FinalprojectApplication.java
+++ src/main/java/com/finalproj/finalproject/FinalprojectApplication.java
@@ -1,13 +1,26 @@
 package com.finalproj.finalproject;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Hello world!
+ */
+public class App
+{
 
-@SpringBootApplication
-public class FinalprojectApplication {
+    private final String message = "Hello World!";
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinalprojectApplication.class, args);
-	}
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    public static int calc(int a, int b) {
+        int result = a * b;
+        return  result;
+    }
+
+    private final String getMessage() {
+        return message;
+    }
 
 }
