9ad969184bbec7412aa777e65a4e7030b9a58cb2
usridzero <70239463+usridzero@users.noreply.github.com>
2020-11-27 21:30:08 +0000

    Update FinalprojectApplication.java
    another test.

--- src/main/java/com/finalproj/finalproject/FinalprojectApplication.java
+++ src/main/java/com/finalproj/finalproject/FinalprojectApplication.java
@@ -18,7 +18,7 @@ public class FinalprojectApplication {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+        System.out.println(new FinalprojectApplication().getMessage());
     }
 
     private final String getMessage() {
