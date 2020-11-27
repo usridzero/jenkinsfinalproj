a55d440e56e52274015238559c2070daf1b52b52
student <usridzero@gmail.com>
2020-11-27 21:38:00 +0000

    pwd

--- src/main/java/com/finalproj/finalproject/FinalprojectApplication.java
+++ src/main/java/com/finalproj/finalproject/FinalprojectApplication.java
@@ -3,26 +3,11 @@ package com.finalproj.finalproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- */
 @SpringBootApplication
 public class FinalprojectApplication {
 
-    private final String message = "Hello World!";
-
-    public FinalprojectApplication() {}
-
-    public static void main(String[] args) {
-	SpringApplication.run(FinalprojectApplication.class, args);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
-
-    private final String getMessage() {
-        return message;
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FinalprojectApplication.class, args);
+	}
 
 }
