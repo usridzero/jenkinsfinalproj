7e8e9993a3207cdadfbb14759f746e17858e0f1e
student <usridzero@gmail.com>
2020-11-27 21:39:02 +0000

    reversing all tests.

--- src/test/java/com/finalproj/finalproject/FinalprojectApplicationTests.java
+++ src/test/java/com/finalproj/finalproject/FinalprojectApplicationTests.java
@@ -3,43 +3,11 @@ package com.finalproj.finalproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-/**
- * Unit test for simple App.
- */
 @SpringBootTest
 class FinalprojectApplicationTests {
-{
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @Test
-    public void testAppConstructor() {
-        try {
-            new App();
-        } catch (Exception e) {
-            fail("Construction failed.");
-        }
-    }
-
-    @Test
-    public void testAppMain()
-    {
-        App.main(null);
-        try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World! \"");
-        }
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
