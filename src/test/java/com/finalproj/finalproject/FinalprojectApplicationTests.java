0dbea3f100557fb98ac603d9f5317ce95aa327e1
student <usridzero@gmail.com>
2020-11-27 20:56:42 +0000

    Adding new pages and test.

--- src/test/java/com/finalproj/finalproject/FinalprojectApplicationTests.java
+++ src/test/java/com/finalproj/finalproject/FinalprojectApplicationTests.java
@@ -1,13 +1,60 @@
 package com.finalproj.finalproject;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import static org.junit.Assert.*;
 
-@SpringBootTest
-class FinalprojectApplicationTests {
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
 
-	@Test
-	void contextLoads() {
-	}
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testAppConstructor() {
+        try {
+            new App();
+        } catch (Exception e) {
+            fail("Construction failed.");
+        }
+    }
+
+    @Test
+    public void testAppMain()
+    {
+        App.main(null);
+        try {
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+        } catch (AssertionError e) {
+            fail("\"message\" is not \"Hello World! \"");
+        }
+    }
+
+    @Test
+    public void testAppCalc()
+    {
+        try {
+            int res=App.calc(5,10);
+            int expected=5*10;
+            assertEquals(expected, res);
+        } catch (AssertionError e1) {
+            fail("Not 50");
+        }
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
 
 }
