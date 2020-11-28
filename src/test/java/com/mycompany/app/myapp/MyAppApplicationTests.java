e86cdd15f6836fa16baa369081d7bf2ec96a1571
student <usridzero@gmail.com>
2020-11-28 00:08:47 +0000

    Fresh install.

---
+++ src/test/java/com/mycompany/app/myapp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
